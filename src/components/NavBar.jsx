e32805a82991f409a758bf4698f0f38c3b859d30
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-01-19 00:18:40 +0200

    signature added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Ampodo</h1>
+      <img className="ml-2 h-12" alt="" src={process.env.PUBLIC_URL + '/icons/a-logo.png'}/>
       </div>
 
       <ul className="hidden md:flex">
