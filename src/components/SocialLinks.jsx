3f81f56a83980e23aa7def6ae39a5b61b18a817e
Vladimir Skrjabin <skrjabingrail@gmail.com>
2023-05-29 15:18:30 +0300

    resume added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resumetech.pdf",
+      href: "/resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
